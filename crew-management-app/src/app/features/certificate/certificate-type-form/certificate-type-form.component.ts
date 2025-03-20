787a5426da0352a3644773130c1fe02b32f29ee4
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2025-03-20 13:40:40 +0300

    New Modal for adding certificate is implemented

--- crew-management-app/src/app/features/certificate/certificate-type-form/certificate-type-form.component.ts
+++ crew-management-app/src/app/features/certificate/certificate-type-form/certificate-type-form.component.ts
@@ -1,21 +1,53 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CertificateType } from '@shared/models/certificate-type.model';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-certificate-type-form',
+  selector: 'app-certificate-type-modal',
   standalone: true,
-  templateUrl: './certificate-type-form.component.html',
-  styleUrl: './certificate-type-form.component.scss',
   imports: [
     CommonModule,
-    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
-    MatTableModule
+    TranslateModule,
+    MatDialogModule
   ],
+  templateUrl: './certificate-type-form.component.html',
+  styleUrls: ['./certificate-type-form.component.scss']
 })
-export class CertificateTypeFormComponent {
+export class CertificateTypeModalComponent {
+  certificateTypeForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CertificateTypeModalComponent>
+  ) {
+    this.certificateTypeForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    console.log('Certificate Type Modal Component initialized');
+  }
+
+  onSubmit() {
+    if (this.certificateTypeForm.valid) {
+      const newCertificateType: CertificateType = {
+        ...this.certificateTypeForm.value,
+        id: Date.now() // Benzersiz bir ID oluştur
+      };
+      this.dialogRef.close(newCertificateType);
+    }
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
